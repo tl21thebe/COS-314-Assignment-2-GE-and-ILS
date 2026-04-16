279885dc6043fd38e17487ca9faadff44a2ebdb2
GiftMHB <giftmohuba159@gmail.com>
2026-04-16 13:18:48 +0200

    iterated local search completed

--- src/IteratedLocalSearch.java
+++ src/IteratedLocalSearch.java
@@ -94,18 +94,93 @@ public class IteratedLocalSearch {
     }
     private int[] localSearch(int[] solution){
 
+        int[] current = copyArray(solution);
+
+        int currentValue = evaluate(current);
+
+        boolean improved = true;
+
+        while(improved){
+
+            improved = false;
+            for(int i = 0; i < current.length; i++){
+
+                current[i] = 1 - current[i];
+                int neighbourValue = evaluate(current);
+
+                if (isValid(current) && neighbourValue > currentValue) {
+
+                   currentValue = neighbourValue;
+                   improved = true; 
+                } else{
+                   current[i] = 1 - current[i]; 
+                }
+            }
+        }
+        return current;
     }
     private int[] perturb(int[] solution){
 
+        int[] perturbed = copyArray(solution);
+
+        //we can experiment with this for better result
+        int k = 3;
+
+        for(int i = 0; i < k; i++){
+          
+            int randomIndex = random.nextInt(perturbed.length); 
+            perturbed[randomIndex] = 1 - perturbed[randomIndex];
+        }
+
+        while (!isValid(perturbed)) {
+
+            // Find all items currently IN the knapsack
+            java.util.List<Integer> includedItems = new java.util.ArrayList<>();
+            for (int i = 0; i < perturbed.length; i++) {
+                if (perturbed[i] == 1) {
+                    includedItems.add(i);
+                }
+            }
+
+            if (includedItems.isEmpty()) break;
+
+            // Remove a random included item
+            int randomIndex = random.nextInt(includedItems.size());
+            perturbed[includedItems.get(randomIndex)] = 0;
+        }
+
+        return perturbed;
     }
     private int evaluate(int[] solution){
 
+        int totalValue = 0;
+
+        for (int i = 0; i < solution.length; i++){
+            if (solution[i] == 1){
+                totalValue += instance.getValues()[i];
+            }
+        }
+        return totalValue;
     }
     private boolean isValid(int[] solution){
 
+        int totalWeight = 0;
+
+        for (int i = 0; i < solution.length; i++){
+            if (solution[i] == 1){
+                totalWeight += instance.getWeights()[i];
+            }
+        }
+        return totalWeight <= instance.getCapacity();
     }
     private int[] copyArray(int[] solution){
 
+       int[] copy = new int[solution.length];
+       
+       for (int i = 0; i < solution.length; i++){
+            copy[i] = solution[i];
+       }
+       return copy;
     }
     
 }
